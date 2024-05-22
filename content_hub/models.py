438d34ec0670ca70b0a3350a26e394682c3656a2
TalhaAnik56 <aniktalha@gmail.com>
2024-05-22 21:37:17 +0600

    question, answer and like models are created

--- content_hub/models.py
+++ content_hub/models.py
@@ -1,5 +1,8 @@
 import markdown
+from django.conf import settings
 from django.db import models
+
+USER = settings.AUTH_USER_MODEL
 
 # Create your models here.
 
@@ -12,3 +15,26 @@ class Article(models.Model):
     @property
     def content(self):
         return markdown.markdown(self.raw_content, extensions=["extra"])
+
+
+# image will be added later
+class Question(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(USER, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    verified = models.BooleanField(default=False)
+    user = models.ForeignKey(USER, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(USER, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["answer", "user"]
