6ec0cec971031ec097b9599c7a2f3d9a0ee2482c
TalhaAnik56 <aniktalha@gmail.com>
2024-05-24 07:29:06 +0600

    added audience and doctors to admin page

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.Audience)
+class AudienceAdmin(admin.ModelAdmin):
+    list_display = ["user", "gender", "age", "education", "address", "created_at"]
+
+
+@admin.register(models.Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "gender",
+        "age",
+        "address",
+        "medical_college",
+        "degree",
+        "year_of_passing",
+        "verified",
+        "created_at",
+    ]
+
+    list_editable = ["verified"]
