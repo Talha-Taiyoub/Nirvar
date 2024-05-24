08cecbb6c3fc70816a602b53ce45721d1bfc36ba
TalhaAnik56 <aniktalha@gmail.com>
2024-05-24 07:18:24 +0600

    changed the header file of the admin

--- nirvar/urls.py
+++ nirvar/urls.py
@@ -20,6 +20,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "Nirvar Administration"
+admin.site.index_title = "Admin"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
